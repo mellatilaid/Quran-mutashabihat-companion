793dfdc522114776d2f264e90cb3977de36bf803
mellati laid <laidmellati@gmail.com>
2026-07-28 18:16:34 +0100

    feat: Add AppDimensionsTheme extension for improved theming support

--- lib/src/core/extensions/theme_extension.dart
+++ lib/src/core/extensions/theme_extension.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:quran_mutashibihat_app/src/core/extensions/app_dimentions.dart';
 
 extension ThemeExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => theme.textTheme;
   ColorScheme get colorScheme => theme.colorScheme;
+  AppDimensionsTheme get dimensionsTheme =>
+      theme.extension<AppDimensionsTheme>()!;
 }
